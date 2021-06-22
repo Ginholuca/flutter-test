d2ce7b697589176ccfe2afb651cbf4a69f13d538
Giovanni De Luca <ginholucacorrea@icloud.com>
2021-06-22 04:40:41 -0300

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,43 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-        appBar: AppBar(
-      title: const Text('MaterialApp Theme'),
-    )),
     theme:
         ThemeData(brightness: Brightness.dark, primaryColor: Colors.blueGrey),
+    home: HomePage(),
   ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contando Flutter'),
+      ),
+      body: Center(
+        child: Text(
+          "Contador\n$count",
+          textAlign: TextAlign.center,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
 }
